7da4f6ab8689b7a4ca4a242d592e78c1910b52d4
Dengoan969 <dengoan969@gmail.com>
2023-06-19 23:06:49 +0500

    rinat want it

--- public/scripts/getTeamsSkills.js
+++ public/scripts/getTeamsSkills.js
@@ -1,7 +1,7 @@
 const skills = document.getElementById('templateSkills').innerHTML;
 const templateSkills = Handlebars.compile(skills);
 
-fetch('/api/skills')
+fetch('/api/skills/teamsSkills')
     .then(r => r.json())
     .then(data => {
         document.getElementById('skills-list').innerHTML = templateSkills({skills: data})
--- routes/api/skillsAPI.js
+++ routes/api/skillsAPI.js
@@ -24,5 +24,26 @@ router.get("/", async (req, res) => {
     ]).then(skills => res.json(skills));
 });
 
+router.get("/teamsSkills", async (req, res) => {
+    await SkillsDB.aggregate([
+        {
+            $lookup: {
+                from: 'teams',
+                let: { skillName: '$name' },
+                pipeline: [
+                    { $unwind: '$skills' },
+                    { $match: { $expr: { $eq: ['$skills', '$$skillName'] } } },
+                    { $group: { _id: '$skills', count: { $sum: 1 } } }
+                ],
+                as: 'count'
+            }
+        },
+        {
+            $addFields: {
+                count: { $ifNull: [{ $arrayElemAt: ['$count.count', 0] }, 0] }
+            }
+        }
+    ]).then(skills => res.json(skills));
+});
 
 module.exports = router;
