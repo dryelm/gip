58eba0918140eb63ce38029f9c22cd1f45b4e5ef
Dengoan969 <dengoan969@gmail.com>
2023-06-20 16:35:59 +0500

    add create team href and fixes

--- routes/teams.js
+++ routes/teams.js
@@ -4,9 +4,20 @@ const router = express.Router();
 const Teams = require('../models/TeamsDB');
 
 router.get('/', async (req, res) => {
-    //const teams = await Teams.find();
-    // TODO: как вывести имена людей в команде?
-    await res.sendFile(path.join(`${__dirname}`, '..', 'views', 'TeamSearch', 'teamSearch.html'));
+    try {
+        res.sendFile(path.join(`${__dirname}`, '..', 'views', 'TeamSearch', 'teamSearch.html'));
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
+});
+
+router.get("/create", async (req, res) => {
+    if (!req.isAuthenticated()){
+        await res.redirect('/login');
+        return;
+    }
+
+    await res.sendFile(path.join(`${__dirname}`, '..', 'views', 'CreateTeam', 'createTeam.html'));
 });
 
 module.exports = router;
