2ff8b27011aeb73ad4e71632124d9de7f0a899e6
Dengoan969 <dengoan969@gmail.com>
2023-06-22 19:12:13 +0500

    almost add editing save

--- public/scripts/profile.js
+++ public/scripts/profile.js
@@ -14,11 +14,25 @@ function makeEditable(field) {
     saveButton.textContent = 'Сохранить';
     field.appendChild(saveButton);
     // обрабатываем нажатие на кнопку "Сохранить"
-    saveButton.addEventListener('click', function() {
+    saveButton.addEventListener('click', async function() {
         // получаем новое значение поля
         let newValue = input.value;
         // отправляем новое значение на сервер
-        // ...
+        let data = {
+            name: document.querySelector('.username h2') ? document.querySelector('.username h2').textContent : document.querySelector('.username input').value,
+            email: document.querySelector('.email p') ? document.querySelector('.email p').textContent : document.querySelector('.email input').value,
+            telegram: document.querySelector('.telegram p') ? document.querySelector('.telegram p').textContent : document.querySelector('.telegram input').value,
+            about: document.querySelector('.about p') ? document.querySelector('.about p').textContent : document.querySelector('.about input').value,
+            skills: Array.from(document.querySelectorAll('.skills p')).map(skill => skill.textContent).join(',')
+        };
+        data[field.className] = newValue;
+        await fetch('/users', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        });
         // заменяем элемент input на p или h2 с обновленным значением
         let newElement = document.createElement(oldElement.tagName);
         newElement.textContent = newValue;
@@ -32,9 +46,9 @@ function makeEditable(field) {
 let editButtons = document.querySelectorAll('.edit-button');
 // добавляем обработчики событий для всех кнопок "Изменить"
 for (let i = 0; i < editButtons.length; i++) {
-    editButtons[i].addEventListener('click', function() {
+    editButtons[i].addEventListener('click', function(event) {
         // получаем поле, которое нужно отредактировать
-        let field = this.closest('.data-field');
+        let field = this.closest('.data-field > div');
         // делаем поле редактируемым
         makeEditable(field);
     });
