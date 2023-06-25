f69616f7a71b71c1d7d9f0da37116db5ee2ce7ac
dryelm <rinat.gilemshinn@gmail.com>
2023-06-26 03:03:58 +0500

    added some decoration to login and register

--- routes/login.js
+++ routes/login.js
@@ -5,19 +5,23 @@ const path = require('path');
 
 
 router.get('/', async function (req, res) {
+    const error = req.query.error;
     if (req.isAuthenticated()){
         res.redirect('/');
         return;
     }
+    if (error === 'user_not_found') {
+        res.render('login.hbs', {error: 'Пользователь не найден'});
+        return;
+    }
 
-
-    res.sendFile(path.join(`${__dirname}`, '..', 'views', 'login.html'));
+    res.render('login.hbs');
 });
 
 router.post('/', async function (req, res) {
     passport.authenticate('local', {failureRedirect: '/login'}, (err, user, info) => {
         if (err) { return next(err); }
-        if (!user) { return res.redirect('/login'); }
+        if (!user) { return res.redirect('/login?error=user_not_found'); }
         const returnTo = req.session.returnTo || '/'
         req.logIn(user, function(err) {
             if (err) { return next(err); }
--- routes/register.js
+++ routes/register.js
@@ -6,12 +6,32 @@ const {hashPassword: hashPassword} = require('../hashing');
 const passport = require("passport");
 
 router.get('/', async (req, res) => {
+    const error = req.query.error;
     if (await req.isAuthenticated()){
         res.redirect('/');
         return;
     }
-
-    await res.sendFile(path.join(`${__dirname}`, '..', 'views', 'register.html'));
+    let errorText = "";
+    switch (error) {
+        case "empty_data":
+            errorText = "Заполните все поля";
+            break;
+        case "incorrect_username":
+            errorText = "Некорректное имя пользователя";
+            break;
+        case "incorrect_password":
+            errorText = "Некорректный пароль";
+            break;
+        case "incorrect_email":
+            errorText = "Некорректный email";
+            break;
+        case "user_exist":
+            errorText = "Пользователь с таким именем или email уже существует";
+            break;
+        default:
+            errorText = "";
+    }
+    await res.render('register.hbs', {error: errorText});
 })
 
 router.post('/', async (req, res) => {
@@ -24,22 +44,22 @@ router.post('/', async (req, res) => {
 
     // проверка на валидность данных
     if (!username || !email || !password) {
-        await res.redirect('/register');
+        await res.redirect('/register?error=empty_data');
         return;
     }
 
     if ((username.length < 4 || username.length > 20) || !(/^[a-zA-Z0-9]+$/.test(username))) {
-        await res.redirect('/register');
+        await res.redirect( '/register?error=incorrect_username');
         return;
     }
 
     if (password.length < 4 || password.length > 20) {
-        await res.redirect('/register');
+        await res.redirect('/register?error=incorrect_password');
         return;
     }
 
     if (!(/^[a-zA-Z0-9]+@[a-zA-Z0-9]+\.[a-zA-Z0-9]+$/.test(email))) {
-        await res.redirect('/register');
+        await res.redirect( '/register?error=incorrect_email');
         return;
     }
 
@@ -47,7 +67,7 @@ router.post('/', async (req, res) => {
     try {
         const isExist = await UsersDB.findOne({$or: [{username: username}, {email: email}]});
         if (isExist) {
-            res.redirect('/register');
+            res.redirect(403, '/register?error=user_exist');
             return;
         }
         const user = new UsersDB({ username: username, email: email, password: hashedPassword });
